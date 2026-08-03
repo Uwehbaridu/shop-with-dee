1f87521452d4641492628e075905a8c2060c00eb
Uweh Bariduanen <114431610+Uwehbaridu@users.noreply.github.com>
2026-08-03 19:24:52 +0100

    Wire up routes: home and product detail pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./cart";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -7,22 +8,34 @@ import Showroom from "./components/Showroom";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import CartDrawer from "./components/CartDrawer";
+import ProductPage from "./pages/ProductPage";
+
+function HomePage() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <Products />
+        <About />
+        <Showroom />
+        <Contact />
+      </main>
+      <Footer />
+      <CartDrawer />
+    </div>
+  );
+}
 
 export default function App() {
   return (
-    <CartProvider>
-      <div className="min-h-screen">
-        <Header />
-        <main>
-          <Hero />
-          <Products />
-          <About />
-          <Showroom />
-          <Contact />
-        </main>
-        <Footer />
-        <CartDrawer />
-      </div>
-    </CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+        </Routes>
+      </CartProvider>
+    </BrowserRouter>
   );
 }
